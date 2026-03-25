9dbf7d3656cd1df46e0a22a0d1f45c09fefeeb84
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-03-25 18:25:53 +0200

    Add GameHandler to ServerScriptService and remove init.server.luau

---
+++ src/server/GameHandler.server.lua
@@ -0,0 +1,11 @@
+local gameStatus = game.ReplicatedStorage:WaitForChild("GameStatus")
+
+local lobbyCFrame = workspace:WaitForChild("Lobby"):WaitForChild("SpawnLocation").CFrame + Vector3.yAxis*5
+
+local INTERMISSION = 10
+
+while true do
+
+    -- Intermission
+
+end
