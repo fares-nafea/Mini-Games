f56ef6fdde0bc0be9bc042880834a5f4736329a5
Fares Ahmed <fares.nafe3.12@gamil.com>
2026-04-03 23:45:08 +0200

    Add item management to player data: implement ItemDataStore for saving and loading player items, and create structure for current item and power tracking.

--- src/server/Leaderstatus.server.lua
+++ src/server/Leaderstatus.server.lua
@@ -2,6 +2,7 @@ local Players = game:GetService("Players")
 
 local DataStoreService = game:GetService("DataStoreService")
 local WinsDataStore = DataStoreService:GetOrderedDataStore("WinsData-0")
+local ItemDataStore = DataStoreService:GetDataStore("Items-0")
 
 Players.PlayerAdded:Connect(function(player)
 
@@ -11,12 +12,37 @@ Players.PlayerAdded:Connect(function(player)
     local winsStat = Instance.new("NumberValue", leaderstatsFolder)
     winsStat.Name = "Wins"
 
+    local itemsFolder = Instance.new("Folder", player)
+    itemsFolder.Name = "Items"
+
+    local currentItem = Instance.new("ObjectValue", player)
+    currentItem.Name = "CurrentItem"
+
+    local currentPower = Instance.new("BoolValue", player)
+    currentPower.Name = "CurrentPower"
+
     -- Load data if it exits
     local plrWinsData = WinsDataStore:GetAsync(player.UserId)
 
     if plrWinsData then
         winsStat.Value = plrWinsData
     end
+
+    local itemsData = ItemDataStore:GetAsync(player.UserId)
+
+    if itemsData then
+        for i, savedItem in ipairs(itemsData) do
+
+            local itemValue = Instance.new("StringValue", itemsFolder)
+            itemValue.Name = savedItem
+
+        end
+    end
+
+    player.CharacterAdded:Connect(function()
+        currentItem.Value = nil
+        currentPower.Value = nil
+    end)
 
 end)
 
@@ -25,6 +51,17 @@ local function SaveData(player)
     local currentWins = player.leaderstats.Wins.Value
     WinsDataStore:SetAsync(player.UserId, currentWins)
 
+    -- Saved Items
+    local savedItems = {}
+
+    local playersItems = player.Items:GetChildren()
+    for i, itemValue in ipairs(playersItems) do
+
+        table.insert(savedItems, itemValue.Name)
+
+    end
+
+    ItemDataStore:SetAsync(player.UserId, savedItems)
 end
 
 Players.PlayerRemoving:Connect(SaveData)
